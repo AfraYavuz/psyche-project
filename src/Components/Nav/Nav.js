625ba8dd084b324ed631722160fee5cd2322a2ee
AfraYavuz <afra_yavuz@hotmail.com>
2022-12-23 21:16:57 +0300

    navbar & hamburger menu

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,6 +6,18 @@ import "./Nav.css";
 
 const Nav = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
 
   return (
     <Router>
@@ -13,6 +25,50 @@ const Nav = () => {
         <div className="nav-container">
           <div className="navbar">
             <div className="logo">PSYCHE</div>
+
+            <ul className={active}>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Anasayfa
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Hakkımızda
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Psikoloji Hakkında
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Psikolojik Testler
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Doktorlar
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  S.S.S
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  İletişim
+                </a>
+              </li>
+            </ul>
+            <div onClick={navToggle} className={icon}>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+            </div>
+
             <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
               <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
                 <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
